db848c530324353c88c5ced28e7850afbb2ea3ae
pathumm1234 <156317647+pathumm1234@users.noreply.github.com>
2024-06-06 14:35:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.antitags = true;
 global.autoTyping = false;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = false;
 
 
@@ -21,15 +21,15 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
-global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
-global.author = "SACHITH";                               //Do not change.
+global.Owner = ["+94773416478", "+94773416478"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94773416478", "+94773416478"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94773416478"];
+global.OwnerName = "𝙋𝙖𝙩𝙝𝙪𝙢 𝙈𝙖𝙡𝙨𝙖𝙧𝙖";
+global.BotName = "𝙋𝙖𝙩𝙝𝙪𝙢 𝙈𝙖𝙡𝙨𝙖𝙧𝙖";
+global.packname = "😈 │𝐃𝐀𝐑𝐊│𝐒𝐇𝐀𝐃𝐎𝐖│𝐌𝐃│𝐕➂ 😈";                             //Do not change.
+global.author = "pathum";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.SupportGroupLink = "https://chat.whatsapp.com/D3hiUqAYYKQ5yGt8p5L920";
 
 
 //
